4f07344114d7ef2309d684a876e41a8ea359c334
Yuncheng Li (yli) <raingomm@gmail.com>
2014-04-29 19:52:31 -0400

    several bug fix

--- local_settings.R
+++ local_settings.R
@@ -1,4 +1,4 @@
-base.dir <- '/scratch/yli-data/bst512-sim/'
+base.dir <- '/scratch/yli-data/bst512-sim-more/'
 n.core <- 12
 
 if (!file.exists(base.dir))
--- test-cluster.R
+++ test-cluster.R
@@ -2,9 +2,9 @@
 library(doSNOW)
 
 getnodes <- function(hosts) {
-    #f <- Sys.getenv('PBS_NODEFILE')
-    #x <- if (nzchar(f)) readLines(f) else rep('localhost', 3)
-    x <- hosts
+    f <- Sys.getenv('PBS_NODEFILE')
+    x <- if (nzchar(f)) readLines(f) else rep('localhost', 3)
+    #x <- hosts
     as.data.frame(table(x), stringsAsFactors=FALSE)
 }
 
@@ -19,12 +19,16 @@ setcores <- function(cl, nodes) {
 }
 setcores(cl, nodes)
 
+    cwd <- getwd()
+    cat(cwd, '\n')
+
 r <- foreach(i=seq_along(nodes$x), .packages='doMC') %dopar% {
     registerDoMC(allocated.cores)
     ppid <- Sys.getpid()
+            setwd(cwd)
     foreach(j=1:allocated.cores, .combine='c', .packages='R.utils') %dopar% {
         Sys.sleep(1)
-        c(ppid, Sys.getpid(), System$getHostname())
+        c(ppid, Sys.getpid(), System$getHostname(), getwd())
     }
 }
 stopCluster(cl)
